c972f3df33695406494adec46ba95e3a7b075152
Marcos Juan <mjuan@thehotelsnetwork.com>
2024-01-23 11:04:21 +0100

    fix: buffermanager show path

--- lua/user/buffermanager.lua
+++ lua/user/buffermanager.lua
@@ -1,12 +1,12 @@
 local M = {
-  "j-morano/buffer_manager.nvim"
+  "j-morano/buffer_manager.nvim",
 }
 
 M.config = function()
   local opts = { noremap = true }
   local map = vim.keymap.set
   -- Setup
-  require("buffer_manager").setup({
+  require("buffer_manager").setup {
     select_menu_item_commands = {
       v = {
         key = "<C-v>",
@@ -18,12 +18,12 @@ M.config = function()
       },
     },
     focus_alternate_buffer = true,
-    short_file_names = true,
-    short_term_names = false,
+    short_file_names = false,
+    short_term_names = true,
     loop_nav = true,
-  })
+  }
   -- Navigate buffers bypassing the menu
-  local bmui = require("buffer_manager.ui")
+  local bmui = require "buffer_manager.ui"
   local keys = "1234567890"
   for i = 1, #keys do
     local key = keys:sub(i, i)
@@ -38,17 +38,17 @@ M.config = function()
     bmui.toggle_quick_menu()
     -- wait for the menu to open
     vim.defer_fn(function()
-      vim.fn.feedkeys("/")
+      vim.fn.feedkeys "/"
     end, 50)
   end, opts)
   -- Next/Prev
   map("n", "<leader>j", bmui.nav_next, opts)
   map("n", "<leader>k", bmui.nav_prev, opts)
   -- Reordering
-  vim.api.nvim_command([[
+  vim.api.nvim_command [[
       autocmd FileType buffer_manager vnoremap J :m '>+1<CR>gv=gv
       autocmd FileType buffer_manager vnoremap K :m '<-2<CR>gv=gv
-  ]])
+  ]]
 end
 
 return M
