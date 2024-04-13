993b6b5b859960785d81870482c2b1bd3acd332b
Marcos Juan <mjuan@thehotelsnetwork.com>
2024-04-13 13:12:46 +0200

    fix(bufferline): remove extra separator characte

--- lua/user/plugins/bufferline.lua
+++ lua/user/plugins/bufferline.lua
@@ -1,21 +1,31 @@
 return {
-  'akinsho/bufferline.nvim',
-  version = '*',
-  dependencies = 'nvim-tree/nvim-web-devicons',
-  config = function()
-    local icons = require 'user.icons'
-    require('bufferline').setup {
-      options = {
-        diagnostics = false, -- 'nvim_lsp', -- show lsp diagnostics
-        mappings = true,
-        show_buffer_close_icons = false,
-        show_close_icon = false,
-        show_tab_indicators = false,
-        separator_style = 'thin',
-        always_show_bufferline = true,
-        modified_icon = icons.ui.Dot,
-        diagnostics_indicator = false,
-      },
-    }
-  end,
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		local icons = require("user.icons")
+		require("bufferline").setup({
+			options = {
+				diagnostics = false, -- "nvim_lsp", -- show lsp diagnostics
+				mappings = true,
+				show_buffer_close_icons = false,
+				show_close_icon = false,
+				show_tab_indicators = false,
+				separator_style = { "", "" },
+				always_show_bufferline = true,
+				modified_icon = icons.ui.Dot,
+				diagnostics_indicator = true,
+				enforce_regular_tabs = false,
+				indicator = {
+					style = "icon",
+					icon = " ",
+					buffer_close_icon = "",
+					modified_icon = icons.ui.Dot,
+					close_icon = icons.ui.BoldClose,
+					left_trunc_marker = icons.ui.ArrowCircleLeft,
+					right_trunc_marker = icons.ui.ArrowCircleRight,
+				},
+			},
+		})
+	end,
 }
